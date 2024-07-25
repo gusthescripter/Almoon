b2d7732ee8f00b1404c58b22c8be613a80eef342
gus <gus.spencer@protonmail.com>
2024-07-25 21:19:39 +0000

    setting up frontend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+
+const Header = () => {
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+				<Container>
+					<Navbar.brand
+	)
+}
