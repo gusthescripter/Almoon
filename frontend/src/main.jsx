e48219667630d1303120a0095436ca538b59a755
gus <gus.spencer@protonmail.com>
2024-09-08 00:50:09 +0000

    adding slices for redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import store from './store';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -11,19 +13,22 @@ import {
 } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen.jsx';
-
+import RegisterScreen from './screens/RegisterScreen.jsx';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/' element={ <App /> }>
 			<Route index={ true } path='/' element={<HomeScreen />} />
 			<Route path='/login' element={<LoginScreen />} />
+			<Route path='/register' element={<RegisterScreen />} />
 		</Route>
 	)
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
 	<React.StrictMode>  
 		<RouterProvider router={ router } />
 	</React.StrictMode>
+    </Provider>
 );
